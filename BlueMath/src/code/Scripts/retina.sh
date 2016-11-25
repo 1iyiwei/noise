d51c9e030811a65b53c9e4d7325ab5058eada707
liyiwei <git@liyiwei.org>
2016-11-25 16:12:38 +0800

    retinal experiment setup

--- BlueMath/src/code/Scripts/retina.sh
+++ BlueMath/src/code/Scripts/retina.sh
@@ -30,8 +30,8 @@ set measure = "isotropic"
 set max_importance_arg = "";
 
 set dot_radius = 0.005;
-set r_value = 0.04;
-set r_text = 0p04;
+set r_value = 0.03;
+set r_text = 0p03;
 set k_value = 0.70;
 #set conflict_metric = 0; # mean
 set boundary = 1; # toroidal
@@ -56,19 +56,21 @@ ${exe_dir}/F2M2PFM ${output_dir}/${domain_option}.f2m ${output_dir}/${domain_opt
 
 else if(${domain_option} == "1blob") then
 
-echo "1 2 1 0.5 0.5 0.3 0.3" > ${output_dir}/${domain_option}.txt
+echo "1 2 1 0.5 0.5 0.2 0.2" > ${output_dir}/${domain_option}.txt
 
-set domain_res_1d = 32;
-set importance_cell_size = 0.03125; # 1.0/32
-
-set domain_res_1d = 8;
-set importance_cell_size = 0.125; # 1.0/8
+set domain_res_1d = 128;
+set importance_cell_size = 0.0078125; # 1.0/128
 
 set domain_res = ${domain_res_1d}x${domain_res_1d};
 
 ${exe_dir}/MakeBlobs ${output_dir}/${domain_option}.txt ${output_dir}/${domain_option}.pfm ${domain_res_1d} 1 ${domain_size};
 
 ${exe_dir}/PFM2F2M ${output_dir}/${domain_option}.pfm ${output_dir}/${domain_option}.f2m
+
+if(0) then
+${exe_dir}/PFM2PPM ${output_dir}/${domain_option}.pfm ${output_dir}/${domain_option}.ppm 1 1 2
+${convert} ${output_dir}/${domain_option}.ppm ${output_dir}/${domain_option}.png
+endif
 
 else
 
@@ -94,7 +96,7 @@ ${exe_dir}/SamplePFT ${huge_dir}/${base_name}_${i}_selected.txt ${huge_dir}/${ba
 
 set all_pfm_files = "${all_pfm_files} ${huge_dir}/${base_name}_${i}.pfm";
 
-set final_samples = `${exe_dir}/SampleSelector ${huge_dir}/${inverse_name}_${i}.txt ${dimension} cout 0`;
+set final_samples = `${exe_dir}/SampleSelector ${huge_dir}/${base_name}_${i}.txt ${dimension} cout 0`;
 @ num_output_samples = $#final_samples / ${dimension};
 
 if(${i} == 0) then
@@ -120,7 +122,6 @@ rm ${output_dir}/all_pfm_files_list.txt;
 ${exe_dir}/PFM2PPM ${output_dir}/${base_name}_average${num_runs}.pfm ${output_dir}/${base_name}_average${num_runs}.ppm 1 1 0
 ${convert} ${output_dir}/${base_name}_average${num_runs}.ppm ${output_dir}/${base_name}_average${num_runs}.png
 echo ${output_dir}/${base_name}_average${num_runs}.png
-
 
 if(${normalization_option} == 'samplecount') then
 set hist_normalization_const = ${num_output_samples};
